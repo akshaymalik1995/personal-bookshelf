d30baace9fb168f66bc8b3b957b460bb0f0fcef0
akshaymalik1995 <akshaymalik191@gmail.com>
2024-06-04 17:13:58 +0530

    Author list curtailed. Form UI Change

--- src/features/search/BookCard.tsx
+++ src/features/search/BookCard.tsx
@@ -9,7 +9,7 @@ export default function BookCard(props: { book: IBook }) {
                 <div className="p-8">
 
                     <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{book.title}</div>
-                    <p className="mt-2 text-gray-500">{book.author_name && book.author_name.length ? book.author_name.join(" , ") : "Not Available"}</p>
+                    <p className="mt-2 text-gray-500">{book.author_name && book.author_name.length ? book.author_name.slice(0, 3).join(" , ") : "Not Available"}</p>
 
 
                     <p className="mt-2 text-gray-500">Ratings: {book.ratings_average ? book.ratings_average.toFixed(2) : "Not available"}</p>
--- src/features/search/Form.tsx
+++ src/features/search/Form.tsx
@@ -17,10 +17,10 @@ export default function Form(props: {queryOn : Boolean, onInputChange: (value: s
             {/* A input form to send request to the api */}
             <form onSubmit={e => e.preventDefault()} action="">
                 <div>
-                    <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Search books</label>
-                    <div className="border items-center flex space-x-2 w-full border-gray-300 text-gray-900 px-2 text-sm rounded-lg" >
-                        <input value={inputValue} onChange={(e) => handleInput(e.target.value)} type="text" id="first_name" className="w-full p-2.5 focus:outline-none rounded-lg " placeholder="The lord of the rings" required />
-                        <div className="bg-white " >{queryOn ? <Spinner /> : ""}</div>
+                    <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900">Search books</label>
+                    <div className="border items-center flex space-x-2 w-full border-gray-300 text-gray-900 bg-blue-100 text-sm rounded-lg" >
+                        <input value={inputValue} onChange={(e) => handleInput(e.target.value)} type="text" id="first_name" className="w-full p-2.5 focus:outline-none active:bg-inherit focus:bg-inherit rounded-lg " placeholder="The lord of the rings" required />
+                        {queryOn ? <div className="px-2 " > <Spinner /> </div> : ""}
                     </div>
                    
                 </div>
