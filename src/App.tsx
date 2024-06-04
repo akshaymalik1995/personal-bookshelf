4beb4437016860623b6f5debb43af5c56d10bcba
akshaymalik1995 <akshaymalik191@gmail.com>
2024-06-04 16:54:22 +0530

    Title added

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,10 @@ function App() {
   return (
     <>
       <div className='max-w-4xl p-4 mx-auto ' >
-        <div className='flex my-8 cursor-pointer ' >
+        <h1 className='text-center text-4xl my-4' >MyShelf</h1>
+        <div className='flex my-12 cursor-pointer ' >
           <div onClick={() => setActiveTab("search")} className={`grow text-center rounded-l p-2 ${activeTab === "search" ? "bg-gray-800 text-white" : "bg-gray-100"} `}>Search</div>
-          <div onClick={() => setActiveTab("shelf")} className={`grow text-center rounded-l p-2 ${activeTab === "shelf" ? "bg-gray-800 text-white " : "bg-gray-100"} `} >Bookshelf</div>
+          <div onClick={() => setActiveTab("shelf")} className={`grow text-center rounded-l p-2 ${activeTab === "shelf" ? "bg-gray-800 text-white " : "bg-gray-100"} `} >Shelf</div>
         </div>
         {activeTab === "search" ? (
           <Query />
